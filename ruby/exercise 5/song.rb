a7c255797f11fa575990abe523c953fb1973396f
astronautaCero <jffersosa@gmail.com>
2016-07-07 10:52:05 -0500

    07/06/2016-10:52am

---
+++ ruby/exercise 5/song.rb
@@ -0,0 +1,35 @@
+class Song
+
+  def initialize(nombre, duracion, artista)
+    @nombre = nombre
+    @duracion = duracion
+    @artista = artista
+  end
+
+  def to_s
+    "El nombre es: #{@nombre} y el Arstita es: #{@artista}"
+  end
+end
+
+class Le < Song
+
+  def initialize(nombre, duracion, artista, letra)
+    super(nombre, duracion, artista)
+    @letra = letra
+  end
+
+  def to_s
+    super + " y la letra es: #{@letra}"
+  end
+
+end
+
+
+#d = Song.new("bru", "3:40", "nn")
+#p d.inspect
+#p d.to_s
+
+x = Le.new("bru", "3:40", "nn", "oh gloria")
+
+p x.inspect
+p x.to_s
